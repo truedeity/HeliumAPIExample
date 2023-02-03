ccc196b8efa6297c8b267002effcdff4efff5ed1
Daniel Newman <dan.newman1982@gmail.com>
2023-02-03 01:12:01 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace HeliumAPIExample
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.helium.io/v1/");
+
+            var response = await client.GetAsync("blockchain");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var blockchainInfo = JsonSerializer.Deserialize<BlockchainInfo>(content);
+
+            Console.WriteLine($"Blockchain height: {blockchainInfo.height}");
+        }
+    }
+
+    class BlockchainInfo
+    {
+        public int height { get; set; }
+    }
+}
